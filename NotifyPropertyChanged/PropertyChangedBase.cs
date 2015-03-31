a383327c8d6c38adcdd72225e170e289b316ca5b
Daniel Cirket <daniel.cirket@gmail.com>
2015-03-31 13:10:03 +0100

    Initial Commit

---
+++ NotifyPropertyChanged/PropertyChangedBase.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Implementations
+{
+    public abstract class PropertyChangedBase : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged Members
+
+        /// <summary>
+        /// Raised when a property on this object has a new value.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raises this object's PropertyChanged event.
+        /// </summary>
+        /// <param name="propertyName">The property that has a new value.</param>
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            if (this.PropertyChanged != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                this.PropertyChanged(this, e);
+            }
+        }
+
+        #endregion
+    }
+}
